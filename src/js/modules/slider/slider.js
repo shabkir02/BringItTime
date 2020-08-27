3ea8ffddbdeaf6dc322b0f0cdf181e435bf06fbe
Kirill <shabkir02@gmail.com>
2020-08-27 21:37:06 +0600

    Difference

---
+++ src/js/modules/slider/slider.js
@@ -0,0 +1,20 @@
+export default class Slider {
+    constructor({
+        container = null, 
+        btns = null, 
+        next = null, 
+        prev = null,
+        activeClass = '',
+        animate,
+        autoplay } = {}) {
+        this.container = document.querySelector(container);
+        this.slides = this.container.children;
+        this.btns = document.querySelectorAll(btns);
+        this.next = document.querySelector(next);
+        this.prev = document.querySelector(prev);
+        this.activeClass = activeClass;
+        this.animate = animate;
+        this.autoplay = autoplay;
+        this.slideIndex = 1;
+    }
+}
